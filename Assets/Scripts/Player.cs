e11a94ec37ab429abd614156753fb3c7e87c7946
Kevin van der Vleuten <kevin.vd.vleuten@gmail.com>
2016-02-01 18:08:02 +0100

    added new player controls using Raycasts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Controller2D))]
+
+public class Player : MonoBehaviour {
+
+    Controller2D controller;
+
+    float moveSpeed = 6;
+    float gravity = -20;
+    Vector3 velocity;
+
+	void Start () {
+        controller = GetComponent<Controller2D>();
+	}
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
